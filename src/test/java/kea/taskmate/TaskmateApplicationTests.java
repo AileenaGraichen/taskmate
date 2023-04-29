7bc68170643d6cd01564104573020ea0cbf9b795
Aileena <aile0074@stud.kea.dk>
2023-04-29 14:00:49 +0200

    setup

---
+++ src/test/java/kea/taskmate/TaskmateApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.taskmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
