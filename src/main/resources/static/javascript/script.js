44a2125c88ccd3c3379411dd7237368771470c91
G1tHubberen <frej3215@stud.kea.dk>
2023-05-17 08:31:36 +0200

    sprint 1 update

--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -7,3 +7,19 @@ function showRegisterForm(){
     document.getElementById("login-form").style.display = "none";
     document.getElementById("register-form").style.display = "block";
 }
+
+function openCreateProjectForm() {
+    document.getElementById("create-project-form").style.display = "block";
+}
+
+function openCreateSectionForm() {
+    document.getElementById("create-section-form").style.display = "block";
+}
+
+function openEditProjectForm() {
+    document.getElementById("edit-project-form").style.display = "block";
+}
+
+function openDeleteProjectPopUp() {
+    document.getElementById("delete-project-id").style.display = "block";
+}
