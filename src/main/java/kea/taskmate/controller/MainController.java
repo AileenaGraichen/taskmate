11023eb79a726f29f0fcbdec58f22f2ccf4d1b35
G1tHubberen <frej3215@stud.kea.dk>
2023-05-18 08:46:25 +0200

    sprint 1 update.2

--- src/main/java/kea/taskmate/controller/MainController.java
+++ src/main/java/kea/taskmate/controller/MainController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
 import java.sql.Date;
 import java.util.List;
 
--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -9,7 +9,7 @@ function showRegisterForm(){
 }
 
 function openCreateProjectForm() {
-    document.getElementById("create-project-form").style.display = "block";
+    document.getElementById("create-project").style.display = "block";
 }
 
 function openCreateSectionForm() {
@@ -22,6 +22,38 @@ function openEditProjectForm() {
 
 function openDeleteProjectPopUp() {
     document.getElementById("delete-project-id").style.display = "block";
+}
+
+function openCreateActivityForm() {
+    document.getElementById("create-activity-form").style.display = "block";
+}
+
+function openEditActivityForm() {
+    document.getElementById("edit-activity-form").style.display = "block";
+}
+
+function openDeleteActivityPopUp() {
+    document.getElementById("delete-activity-id").style.display = "block";
+}
+
+function openEditSectionForm() {
+    document.getElementById("edit-section-form").style.display = "block";
+}
+
+function openDeleteSectionPopUp() {
+    document.getElementById("delete-section-id").style.display = "block";
+}
+
+function openCreateTaskForm() {
+    document.getElementById("create-task-form").style.display = "block";
+}
+
+function openEditTaskForm() {
+    document.getElementById("edit-task-form").style.display = "block";
+}
+
+function openDeleteTaskPopUp() {
+    document.getElementById("delete-task-id").style.display = "block";
 }
 
 
